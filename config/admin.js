2d1ccf26f9ae0430ddfa83684b89b4fd67b721e8
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2022-04-23 11:42:40 -0300

    Set the API token salt as an environment variable

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '49d1b4aeaa0a14ad1633a116c1ff1bdc'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
+  }
 });
